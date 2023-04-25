2b9706042bee4641d2ed0ccde63fd8dcbbded1b5
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2023-04-24 20:47:53 -0700

    fix compilation bugs

--- build.rs
+++ build.rs
@@ -1,12 +1,13 @@
 extern crate rkdb;
 
+#[allow(unused_imports)]
 use rkdb::SYMBOLS;
 
 fn main() {
     //TODO: While SYMBOLS is complete and correct, we can't set -Clink-args via buid.rs as of current Aug 18 nightly
     // currently hard coded in .cargo/config
     /*
-    for s in SYMBOLS.iter() { ... }  
-    println!("cargo:rustc-env=RUSTFLAGS=-Clink-args=-Wl,-U,_krr") 
+    for s in SYMBOLS.iter() { ... }
+    println!("cargo:rustc-env=RUSTFLAGS=-Clink-args=-Wl,-U,_krr")
     */
 }
